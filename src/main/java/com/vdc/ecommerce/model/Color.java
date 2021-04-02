befee339b3a37f2bd793b41d258479ae0b900c44
datvm <vmdat@hanatourjsv.com>
2021-04-02 17:57:03 +0700

    generate product predicate

--- src/main/java/com/vdc/ecommerce/model/Color.java
+++ src/main/java/com/vdc/ecommerce/model/Color.java
@@ -1,9 +1,24 @@
 package com.vdc.ecommerce.model;
 
 public enum Color {
-    RED,
-    BLUE,
-    BLACK,
-    YELLOW,
-    PINK
+    RED("red"),
+    BLUE("blue"),
+    BLACK("black"),
+    YELLOW("yellow"),
+    PINK("pink");
+
+    private final String value;
+
+    Color(String value) {
+        this.value = value;
+    }
+
+    public static Color lookup(String abbr) {
+        for (Color v : values()) {
+            if (v.value.equals(abbr)) {
+                return v;
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/com/vdc/ecommerce/model/predicate/ProductPredicate.java
+++ src/main/java/com/vdc/ecommerce/model/predicate/ProductPredicate.java
@@ -3,6 +3,7 @@ package com.vdc.ecommerce.model.predicate;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
+import com.vdc.ecommerce.model.Color;
 import com.vdc.ecommerce.model.MetricSearch;
 import com.vdc.ecommerce.model.QProduct;
 import org.springframework.stereotype.Component;
@@ -22,32 +23,32 @@ public class ProductPredicate {
         if (!CollectionUtils.isEmpty(metricSearch.getMetricFilters())) {
             for (MetricSearch.MetricFilter o : metricSearch.getMetricFilters()) {
                 if ("name".equals(o.getField())) {
-                    bExpression = bExpression.and(Q_PRODUCT.name.like("%" + o.getValue() + "%"));
+                    bExpression = andNameLike(bExpression, o.getValue());
                 }
                 if ("color".equals(o.getField())) {
-                    bExpression = bExpression.and(Q_PRODUCT.name.like("%" + o.getValue() + "%"));
+                    bExpression = andColorEqual(bExpression, Color.lookup(o.getValue()));
                 }
                 if ("price".equals(o.getField())) {
                     BigDecimal price = o.getValue() == null || o.getValue().isEmpty() ? BigDecimal.ZERO : new BigDecimal(o.getValue());
                     if (o.getCondition().contains(">")) {
-                        bExpression = bExpression.and(Q_PRODUCT.price.goe(price));
+                        bExpression = andPriceMoreThanOrEqual(bExpression, price);
                     } else if (o.getCondition().contains("<")) {
-                        bExpression = bExpression.and(Q_PRODUCT.price.loe(price));
+                        bExpression = andPriceLessThan(bExpression, price);
                     } else {
-                        bExpression = bExpression.and(Q_PRODUCT.price.eq(price));
+                        bExpression = andPriceEqual(bExpression, price);
                     }
                 }
                 if ("branch".equals(o.getField())) {
-                    bExpression = bExpression.and(Q_PRODUCT.branch.name.like("%" + o.getValue() + "%"));
+                    bExpression = andBranchLike(bExpression, o.getValue());
                 }
                 if ("quantity".equals(o.getField())) {
                     Long quantity = o.getValue() == null || o.getValue().isEmpty() ? 0L : Long.parseLong(o.getValue());
                     if (o.getCondition().contains(">")) {
-                        bExpression = bExpression.and(Q_PRODUCT.quantity.quantity.goe(quantity));
+                        bExpression = andQuantityMoreThanOrEqual(bExpression, quantity);
                     } else if (o.getCondition().contains("<")) {
-                        bExpression = bExpression.and(Q_PRODUCT.quantity.quantity.loe(quantity));
+                        bExpression = andQuantityLessThanOrEqual(bExpression, quantity);
                     } else {
-                        bExpression = bExpression.and(Q_PRODUCT.quantity.quantity.eq(quantity));
+                        bExpression = andQuantityEqual(bExpression, quantity);
                     }
                 }
             }
@@ -56,10 +57,71 @@ public class ProductPredicate {
     }
 
     public Predicate findByProductIdIn(List<Long> ids) {
-        if(ids.isEmpty()) {
+        if (ids.isEmpty()) {
             return null;
         }
         return Q_PRODUCT.id.in(ids);
     }
+
+    public BooleanExpression andNameEqual(BooleanExpression predicate, String value) {
+        return predicate.and(Q_PRODUCT.name.eq(value));
+    }
+
+    public BooleanExpression andNameLike(BooleanExpression predicate, String value) {
+        return predicate.and(Q_PRODUCT.name.like("%" + value + "%"));
+    }
+
+    public BooleanExpression andColorEqual(BooleanExpression predicate, Color value) {
+        return predicate.and(Q_PRODUCT.color.eq(value));
+    }
+
+    public BooleanExpression andPriceEqual(BooleanExpression predicate, BigDecimal value) {
+        return predicate.and(Q_PRODUCT.price.eq(value));
+    }
+
+    public BooleanExpression andPriceLessThan(BooleanExpression predicate, BigDecimal value) {
+        return predicate.and(Q_PRODUCT.price.lt(value));
+    }
+
+    public BooleanExpression andPriceLessThanOrEqual(BooleanExpression predicate, BigDecimal value) {
+        return predicate.and(Q_PRODUCT.price.loe(value));
+    }
+
+    public BooleanExpression andPriceMoreThan(BooleanExpression predicate, BigDecimal value) {
+        return predicate.and(Q_PRODUCT.price.gt(value));
+    }
+
+    public BooleanExpression andPriceMoreThanOrEqual(BooleanExpression predicate, BigDecimal value) {
+        return predicate.and(Q_PRODUCT.price.goe(value));
+    }
+
+    public BooleanExpression andBranchEqual(BooleanExpression predicate, String value) {
+        return predicate.and(Q_PRODUCT.name.eq(value));
+    }
+
+    public BooleanExpression andBranchLike(BooleanExpression predicate, String value) {
+        return predicate.and(Q_PRODUCT.name.like("%" + value + "%"));
+    }
+
+    public BooleanExpression andQuantityEqual(BooleanExpression predicate, Long value) {
+        return predicate.and(Q_PRODUCT.quantity.quantity.eq(value));
+    }
+
+    public BooleanExpression andQuantityLessThan(BooleanExpression predicate, Long value) {
+        return predicate.and(Q_PRODUCT.quantity.quantity.lt(value));
+    }
+
+    public BooleanExpression andQuantityLessThanOrEqual(BooleanExpression predicate, Long value) {
+        return predicate.and(Q_PRODUCT.quantity.quantity.loe(value));
+    }
+
+    public BooleanExpression andQuantityMoreThan(BooleanExpression predicate, Long value) {
+        return predicate.and(Q_PRODUCT.quantity.quantity.gt(value));
+    }
+
+    public BooleanExpression andQuantityMoreThanOrEqual(BooleanExpression predicate, Long value) {
+        return predicate.and(Q_PRODUCT.quantity.quantity.goe(value));
+    }
+
 
 }
