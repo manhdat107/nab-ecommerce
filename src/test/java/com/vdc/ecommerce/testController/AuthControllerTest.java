925ae0aca0d9b62e0a9d7c77e5c59f2c661d1cc1
datvm <vmdat@hanatourjsv.com>
2021-04-03 15:41:16 +0700

    add test

---
+++ src/test/java/com/vdc/ecommerce/testController/AuthControllerTest.java
@@ -0,0 +1,30 @@
+package com.vdc.ecommerce.testController;
+
+import com.vdc.ecommerce.model.request.SignInRequest;
+import com.vdc.ecommerce.model.response.ResponseModel;
+import com.vdc.ecommerce.service.IAccountService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class AuthControllerTest {
+//    @InjectMocks
+//    IAccountService accountService;
+//
+//    @Before
+//    public void init() {
+//        MockitoAnnotations.initMocks(this);
+//    }
+    @Test
+    public void test() {
+
+    }
+}
